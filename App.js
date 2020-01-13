565f813143fc02139e01cdcf1bbb88a79b3b6bf6
DJhonJ <diazj691@gmail.com>
2020-01-12 22:18:15 -0500

    iniciando Zira

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Comenzando con zira</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+    padding: 20
+  },
+});
